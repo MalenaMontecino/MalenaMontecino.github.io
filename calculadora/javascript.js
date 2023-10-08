1a10b07d0e0dc087d5f340a7f703bad9423e94eb
Malena Montecino <115707544+MalenaMontecino@users.noreply.github.com>
2023-10-08 23:56:33 +0200

    Add files via upload

---
+++ calculadora/javascript.js
@@ -0,0 +1,53 @@
+function calcularPropina() {
+    var numPersonas= document.getElementById('numPersonas');
+    var costeTotal =  document.getElementById('coste');
+    var calificacionServicio = document.getElementById('calificacionServicio');
+   
+/*        var propinaDefault = "0,50 €" */
+    var porcentaje;
+    var propinaFinal;
+    
+    if(numPersonas.value === ""){
+        numPersonas.value = 1;
+      return false; //porque false
+    }
+    
+    if (costeTotal.value === ""){
+     window.alert("Por favor, ingresa el coste total.");
+      return false;
+    }
+    
+     switch (calificacionServicio.value) {
+         case 'default':
+          window.alert("Por favor seleccione una opción en el menú desplegable");
+        break;
+      case 'genial':
+        porcentaje= 10/100;
+        break;
+      case 'aceptable':
+        porcentaje= 5/100;
+        break;
+      case 'horrible':
+       porcentaje =0;
+       break;
+     }
+     
+     
+     var propinaFinal = (costeTotal.value / numPersonas.value) * porcentaje;
+    
+  if(propinaFinal === 0){
+  propinaFinal = 0.5;
+  }
+    
+    document.getElementById("propina").innerHTML = propinaFinal + " €";
+    
+   
+// QUE SE BORREN LOS DATOS AL FINAL   
+document.getElementById('miFormulario').reset();
+}  
+
+    
+    
+    
+    
+  
