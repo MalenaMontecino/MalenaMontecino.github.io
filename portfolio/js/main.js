d435f6a22f789c786a8f2359323e4692d446e64c
MalenaMontecino <mmontecinom2223@politecnics.barcelona>
2024-06-09 20:44:59 +0200

    fwefwef

--- portfolio/js/main.js
+++ portfolio/js/main.js
@@ -36,3 +36,50 @@ $(".custom-carousel").owlCarousel({
   video.addEventListener('timeupdate', () => {
       updateButtons(video.currentTime);
   });
+
+
+//tooltip
+  document.addEventListener('DOMContentLoaded', function () {
+    var tooltipTriggerList = [].slice.call(document.querySelectorAll('[data-bs-toggle="tooltip"]'))
+    var tooltipList = tooltipTriggerList.map(function (tooltipTriggerEl) {
+      return new bootstrap.Tooltip(tooltipTriggerEl)
+    })
+  });
+
+
+  //toast
+  const toastTrigger = document.getElementById('liveToastBtn')
+const toastLiveExample = document.getElementById('liveToast')
+
+if (toastTrigger) {
+  const toastBootstrap = bootstrap.Toast.getOrCreateInstance(toastLiveExample)
+  toastTrigger.addEventListener('click', () => {
+    toastBootstrap.show()
+  })
+}
+
+ // Función para obtener la hora actual en formato HH:MM AM/PM
+ function getCurrentTime() {
+    const now = new Date();
+    let hours = now.getHours();
+    const minutes = now.getMinutes();
+    const ampm = hours >= 12 ? 'PM' : 'AM';
+    hours = hours % 12;
+    hours = hours ? hours : 12; // La hora '0' debe ser '12'
+    const strTime = hours + ':' + (minutes < 10 ? '0' + minutes : minutes) + ' ' + ampm;
+    return strTime;
+  }
+
+  // Función para establecer la hora actual en el elemento small
+  function setCurrentTime() {
+    document.getElementById('current-time').textContent = getCurrentTime();
+  }
+
+  // Llamar a setCurrentTime cuando se carga la página
+  document.addEventListener('DOMContentLoaded', function () {
+    setCurrentTime();
+    var toastEl = document.getElementById('liveToast');
+    var toast = new bootstrap.Toast(toastEl);
+    toast.show();
+  });
+
